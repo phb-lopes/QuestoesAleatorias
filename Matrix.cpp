13a610d6e11176f78936d06919517cbcfa32ae10
Paulo H. Lopes <paulo_lopes11@hotmail.com>
2019-08-30 12:37:18 -0300

    Add files via upload

---
+++ Matrix.cpp
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <time.h>
+
+int main(){
+	int x=2;
+	int k,i,y,z;
+	while(x>1){
+		system("color 02");
+		srand((unsigned) time(NULL));
+		
+		for(int j=0;j<100;j++){
+		k = rand() % 2;
+		printf("%d",k);
+		i = rand() % 2;
+		printf("%d",i);
+		y = rand() % 2;
+		printf("%d",y);
+		z = rand() % 2;
+		printf("%d",z);
+		k = rand() % 2;
+		printf("%d",k);
+		i = rand() % 2;
+		printf("%d",i);
+		y = rand() % 2;
+		printf("%d",y);
+		z = rand() % 2;
+		printf("%d",z);	
+		k = rand() % 2;
+		printf("%d",k);
+		i = rand() % 2;
+		printf("%d",i);
+		y = rand() % 2;
+		printf("%d",y);
+		z = rand() % 2;
+		printf("%d",z);	
+		k = rand() % 2;
+		printf("%d",k);
+		i = rand() % 2;
+		printf("%d",i);
+		y = rand() % 2;
+		printf("%d",y);
+		z = rand() % 2;
+		printf("%d",z);k = rand() % 2;
+		printf("%d",k);
+		i = rand() % 2;
+		printf("%d",i);
+		y = rand() % 2;
+		printf("%d",y);
+		z = rand() % 2;
+		printf("%d",z);
+		k = rand() % 2;
+		printf("%d",k);
+		i = rand() % 2;
+		printf("%d",i);
+		y = rand() % 2;
+		printf("%d",y);
+		z = rand() % 2;
+		printf("%d",z);	
+		k = rand() % 2;
+		printf("%d",k);
+		i = rand() % 2;
+		printf("%d",i);
+		y = rand() % 2;
+		printf("%d",y);
+		z = rand() % 2;
+		printf("%d",z);	
+		k = rand() % 2;
+		printf("%d",k);
+		i = rand() % 2;
+		printf("%d",i);
+		y = rand() % 2;
+		printf("%d",y);
+		z = rand() % 2;
+		printf("%d",z);		
+		}
+		
+	}
+	return 0;
+	system("PAUSE");
+}
+
